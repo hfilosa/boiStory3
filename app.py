85883f7ab48add0db84e14633c6e9e264dc9d179
Henry Filosa <henryfilosa@henrys-imac.home>
2016-05-24 20:46:38 -0400

    Pulling current annoucnements and submission totally works now

--- app.py
+++ app.py
@@ -10,18 +10,19 @@ def home():
     year=str(datetime.date.today().year)
     month=str(datetime.date.today().month)
     displayDate=month+"/"
-    if month<10:
+    if int(month)<10:
         month="0"+month
     day=str(datetime.date.today().day)
     displayDate=displayDate+day+"/"+year[2:]
-    if day<10:
+    if int(day)<10:
         day="0"+day
     currentDate=year+"-"+month+"-"+day
-
-    currentDate="2016-05-18"
-    displayDate="5/18/16"
+    print currentDate
+    #currentDate="2016-05-18"
+    #displayDate="5/18/16"
     
     announcements=backend.getAnnouncementByDate(currentDate)
+    print 1
     print announcements
     #print announcements[0]
     return render_template("home.html",anonce=announcements,todayDate=displayDate)
@@ -34,7 +35,8 @@ def submit():
         club1=request.form["club"]
         submitter1=request.form["submitter"]
         osis1=request.form["osis"]
-        short1=request.form["short"]
+        title1=request.form["title"]
+        short_form1=request.form["short"]
         long_form1=request.form["long"]
 
         if club1  == "Club/Team Name" or club1 is None:
@@ -46,27 +48,30 @@ def submit():
         if osis1 == "9-Digit OSIS" or osis1 is None:
             errors.append("No OSIS given for who is submitting this announcement")
             osis1 = "9-Digit OSIS"
-        elif len(osis) != 9:
+        if title1 == "Announcement title" or title1 is None:
+            errors.append("No title given for this announcement")
+            title1 = "Announcement title"
+        elif len(osis1) != 9:
             errors.append("An OSIS must be 9 digits long")
-        if short1=="Short form of your announcement. 200 characters maximum" or short1 is None:
+        if short_form1=="Short form of your announcement. 200 characters maximum" or short_form1 is None:
             errors.append("No announcement given")
-            short1="Short form of your announcement. 200 characters maximum"
+            short_form1="Short form of your announcement. 200 characters maximum"
         if long_form1 is None:
             long_form="Optional longer form of your announcement"
         if len(errors)>0:    
-            return render_template("submit.html",error=errors,club=club1,submitter=submitter1,osis=osis1,short_form=short1,long_form=long_form1)
+            return render_template("submit.html",error=errors,club=club1,submitter=submitter1,osis=osis1,title=title1,short_form=short1,long_form=long_form1)
         else:
             year=str(datetime.date.today().year)
             month=str(datetime.date.today().month)
-            if month<10:
+            if int(month)<10:
                 month="0"+month
             day=str(datetime.date.today().day)
-            if day<10:
+            if int(day)<10:
                 day="0"+day
             currentDate=year+"-"+month+"-"+day
             backend.addAnnouncement(club1,submitter1,osis1,title1,short_form1,long_form1,currentDate);
             return redirect("/");
-    return render_template("submit.html",error=errors,club="Club/Team Name",submitter="Name",osis="9-Digit OSIS",short_form="Short form of your announcement. 200 characters maximum",long_form="Optional longer form of your announcement")
+    return render_template("submit.html",error=errors,club="Club/Team Name",submitter="Name",osis="9-Digit OSIS",title="Announcement title",short_form="Short form of your announcement. 200 characters maximum",long_form="Optional longer form of your announcement")
 
 if __name__ == "__main__":
     app.debug = True
--- backend.py
+++ backend.py
@@ -6,9 +6,9 @@ def go():
     if (not os.path.exists(db_name)):
         conn = sqlite3.connect(db_name)
         c = conn.cursor()
-        q = """create table announcements (club text, submitter text, osis integer, title text, short_text text, long_text text, date text, id integer);"""
+        q = """create table announcements (club text, submitter text, osis text, title text, short_text text, long_text text, date text, id integer);"""
         c.execute(q)
-        q = """insert into announcements values ("Dancing Pandas", "Henry Filosa", 208163568, "Announcement Database successfully created", "We did it","This is the long version of this story","2016-05-18",1);"""
+        q = """insert into announcements values ("Dancing Pandas", "Henry Filosa", "208163568", "Announcement Database successfully created", "We did it","This is the long version of this story","2016-05-18",1);"""
         c.execute(q)
         conn.commit()
 
@@ -30,7 +30,7 @@ def addAnnouncement(club, submitter, osis, title, short_text, long_text, date):
     res = c.execute(q)
     for r in res:
         id_num=r[0]+1
-    q = """insert into announcements values ("%s","%s",%i,"%s","%s","%s","%s","%i");"""
+    q = """insert into announcements values ("%s","%s",%s,"%s","%s","%s","%s","%i");"""
     q = q%(club,submitter,osis,title, short_text, long_text, date, id_num)
     c.execute(q)
     conn.commit()
@@ -49,20 +49,20 @@ def printArchive():
     for r in res:
         print r
     
-"""======== Array getAnnouncementByDate(start_date,end_date) ==========
+"""======== Array getAnnouncementByDate(required_date) ==========
 Inputs: date -> Format "YYYY-MM-DD". Must have leading zeroes for date.
          ex. 2016-05-04 is the 4th of may, 2016
 Returns: 
   All archived posts with that date stamp
 """
-def getAnnouncementByDate(date):
+def getAnnouncementByDate(required_date):
     conn = sqlite3.connect(db_name)
     c = conn.cursor()
     q = """SELECT * FROM announcements WHERE DATE(date) == DATE('%s');"""
-    q = q%(date)
+    q = q%(required_date)
     res = c.execute(q)
     ans = []
-    for r in res:
+    for r in c.execute(q):
         ans.append(r)
     return ans
 
